1db10b5715b639262cbe5efdfc5d006e79a9abd4
CELYNEBIRUNGI <113887926+CELYNEBIRUNGI@users.noreply.github.com>
2023-05-02 13:03:09 +0300

    add table

--- script.js
+++ script.js
@@ -1,70 +1,57 @@
-/* eslint-disable max-classes-per-file */
+/*  eslint-disable max-classes-per-file, no-unused-vars */
 class Book {
-  constructor(title, author) {
+  constructor(title, author, id) {
     this.title = title;
     this.author = author;
+    this.id = id;
   }
 }
 
-class BookCollection {
-  constructor() {
-    this.books = JSON.parse(localStorage.getItem('myBooks')) || [];
-    this.bookList = document.getElementById('bookList');
-    this.addBtn = document.getElementById('addButton');
-    this.titleInput = document.getElementById('titleInput');
-    this.authorInput = document.getElementById('authorInput');
+class BookManager {
+  static createBookElement(book) {
+    return `
+      <tr id="${book.id}">
+        <td class="book-info"> "${book.title}" by ${book.author}</td>
+        <td class="remove-button">
+          <button type="button" onclick="removeBookWrapper('${book.id}')">Remove</button>
+        </td>
+      </tr>`;
+  }
 
-    this.addBtn.addEventListener('click', () => this.addBook());
-    this.bookList.addEventListener('click', (event) => {
-      if (event.target.classList.contains('removeButton')) {
-        const index = parseInt(event.target.dataset.index, 10);
-        this.removeBook(index);
-      }
+  static loadData() {
+    const tbody = document.querySelector('tbody');
+    const books = JSON.parse(localStorage.getItem('books')) || [];
+    books.forEach((bookData) => {
+      const book = new Book(bookData.title, bookData.author, bookData.id);
+      tbody.innerHTML += BookManager.createBookElement(book);
     });
-
-    this.renderBookList();
   }
 
   addBook() {
-    const title = this.titleInput.value.trim();
-    const author = this.authorInput.value.trim();
-    const book = new Book(title, author);
-
-    // Add book to collection
-    this.books.push(book);
-
-    // Update local storage
-    localStorage.setItem('myBooks', JSON.stringify(this.books));
-
-    // Clear input fields
-    this.titleInput.value = '';
-    this.authorInput.value = '';
-    this.renderBookList();
+    this.title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const id = new Date().getTime().toString();
+    const book = new Book(this.title, author, id);
+    const tbody = document.querySelector('tbody');
+    tbody.innerHTML += BookManager.createBookElement(book);
+    const books = JSON.parse(localStorage.getItem('books')) || [];
+    books.push({ title: book.title, author: book.author, id: book.id });
+    localStorage.setItem('books', JSON.stringify(books));
   }
 
-  removeBook(index) {
-    // Filter out book at specified index
-    this.books = this.books.filter((book, i) => i !== index);
+  static removeBook(id) {
+    const tbody = document.querySelector('tbody');
+    const book = document.getElementById(id);
+    tbody.removeChild(book);
 
-    // Update local storage
-    localStorage.setItem('myBooks', JSON.stringify(this.books));
-    this.renderBookList();
-  }
-
-  renderBookList() {
-    this.bookList.innerHTML = '';
-    this.books.forEach((book, index) => {
-      // Create book item
-      const bookItem = document.createElement('div');
-      bookItem.classList.add('bookItem');
-      bookItem.innerHTML = `
-        <span>${book.title}&nbsp by &nbsp${book.author}</span>&nbsp &nbsp
-        <button class="removeButton" data-index="${index}">Remove</button>
-      `;
-      this.bookList.appendChild(bookItem);
-    });
+    const books = JSON.parse(localStorage.getItem('books')) || [];
+    const updatedBooks = books.filter((bookData) => bookData.id !== id);
+    localStorage.setItem('books', JSON.stringify(updatedBooks));
   }
 }
 
-/* eslint-disable-next-line no-unused-vars */
-const myBookCollection = new BookCollection();
+const bookManager = new BookManager();
+
+function removeBookWrapper(id) {
+  BookManager.removeBook(id);
+}
